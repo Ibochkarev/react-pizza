26409ab7daafb6093967eb3ebdb42f931dcc914b
Bochkarev Ivan <ivanx86@gmail.com>
2022-06-07 11:37:59 +0600

    Обновление отображения категорий пицц

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,23 @@
+import React, { useState } from "react";
+
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const categories = [ 'Всё','Мясные','Вегетарианская','Гриль','Острые','Закрытые' ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index)
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, i) => (
+          <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''} key={i}>{ value }</li>
+        ))}
       </ul>
     </div>
   )
 }
+
 export default Categories;

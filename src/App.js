7b455d7cb6e2d7b3cf5ccd7d3c2e5b37b906cfd2
Bochkarev Ivan <ivanx86@gmail.com>
2022-06-11 16:01:51 +0600

    Добавлен компонент скелетона

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import { Header, Categories, SortPopup, PizzaBlock } from './components';
+import { Header, Categories, SortPopup } from './components';
+import PizzaBlock from './components/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://62a4571647e6e40063909617.mockapi.io/items')
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
+        setLoading(false);
       });
   }, []);
 
@@ -28,9 +32,10 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock {... obj} key={obj.id} />
-            ))}
+            {isLoading
+              ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => (<PizzaBlock {...obj} key={obj.id} />))
+            }
           </div>
         </div>
       </div>
